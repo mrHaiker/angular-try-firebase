1f36be266500d317eda717c40f30ca50c698983d
sergey.dzyuba <sergey.dzyuba@betconstruct.com>
2018-01-30 00:51:16 +0200

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+var express    = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello world!!!!c')
+});
+
+app.listen(port);
