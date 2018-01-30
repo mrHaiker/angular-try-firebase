c5a0ad986957aee872e3c020d06ed65743a51954
sergey.dzyuba <sergey.dzyuba@betconstruct.com>
2018-01-31 00:31:52 +0200

    Update index file

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-var express    = require('express');
-var app = express();
+var express    = require('express');        // call express
+var bodyParser = require('body-parser');
+// let keys       = require('./keys.json');
 
-var port = process.env.PORT || 3000;
+var app        = express();                 // define our app using express
 
-app.get('/', function (req, res) {
-  res.send('Hello world!!!!c1111')
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;        // set our port
+var PUBLIC_API = 'https://poloniex.com/public';
+var PRIVATE_API = 'https://poloniex.com/tradingApi';
+
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api! 2' });
 });
 
+
+router.get('/returnBalance', function (req, res) {
+  res.json({ message: 'Hello world!!!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
 app.listen(port);
+console.log('Magic happens on port ' + port);
