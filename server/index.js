71e7e01f89b3defaba779f1032e7f45d21f4adb5
sergey.dzyuba <sergey.dzyuba@betconstruct.com>
2018-01-31 23:16:57 +0200

    remove routing in to an index.js file

--- server/index.js
+++ server/index.js
@@ -15,33 +15,6 @@ let PUBLIC_API = 'https://poloniex.com/public';
 let PRIVATE_API = 'https://poloniex.com/tradingApi';
 
 
-// ROUTES FOR OUR API
-// =============================================================================
-let router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api! 2' });
-});
-
-
-router.get('/returnBalance', function (req, res) {
-  axios.get(PUBLIC_API + '?command=returnTicker')
-    .then(function (val) {
-      const data = val.data;
-      res.json(data);
-    })
-    .catch(function (err) {
-      const data = err.response.data;
-      res.send(data);
-    })
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
