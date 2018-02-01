a5298cd38876ea6c73a63d8a89d9fbd18dce1cc8
sergey.dzyuba <sergey.dzyuba@betconstruct.com>
2018-02-01 21:37:30 +0200

    return to first index.js file

--- server/index.js
+++ server/index.js
@@ -20,31 +20,30 @@ let PRIVATE_API = 'https://poloniex.com/tradingApi';
 // let router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function(req, res) {
-//   res.json({ message: 'hooray! welcome to our api! 2' });
-// });
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api! 2' });
+});
 
+router.get('/returnBalance', function (req, res) {
+  axios.get(PUBLIC_API + '?command=returnTicker')
+    .then(function (val) {
+      const data = val.data;
+      res.json(data);
+    })
+    .catch(function (err) {
+      const data = err.response.data;
+      res.send(data);
+    })
+});
 
-// router.get('/returnBalance', function (req, res) {
-//   axios.get(PUBLIC_API + '?command=returnTicker')
-//     .then(function (val) {
-//       const data = val.data;
-//       res.json(data);
-//     })
-//     .catch(function (err) {
-//       const data = err.response.data;
-//       res.send(data);
-//     })
-// });
-//
 // // more routes for our API will happen here
-//
+
 // // REGISTER OUR ROUTES -------------------------------
 // // all of our routes will be prefixed with /api
-// app.use('/api', router);
+app.use('/api', router);
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './test'));
+app.use(express.static(__dirname + '/dist'));
 
 // START THE SERVER
 // =============================================================================
